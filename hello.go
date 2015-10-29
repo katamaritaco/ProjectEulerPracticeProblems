9c42194f91378bcb3ed766baedf6a1caec91ba8e
Nick Heindl <nheindl@wisc.edu>
2015-10-29 00:07:25 -0500

    Add basic windows benchmark to hello.go

--- hello.go
+++ hello.go
@@ -1,5 +1,8 @@
 //hello.go base project to start from
 
+//not the greatest way of benchmarking programs in windows:
+// SET startTime=%time% && go run XXX.go && echo %startTime% && echo %time%
+
 package main
 
 import "fmt"
